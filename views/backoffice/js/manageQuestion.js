1e89e5fcf79d1d70578f7560e7c1d7eef157ee3c
adi <adityaa803@gmail.com>
2016-09-29 18:08:24 +0530

    'login_now_on_server'

--- views/backoffice/js/manageQuestion.js
+++ views/backoffice/js/manageQuestion.js
@@ -1,15 +1,9 @@
 $(document).ready(function() {
-    var ques = {};
-    $('.addQuesBtn').click(function() {
-        ques.question = $(".question").val();
-        ques.level = $(".level").val();
-        ques.techAnswer = $(".techAnswer").val();
-        ques.nonTechAnswer = $(".nonTechAnswer").val();
-        ques.hint = $(".hint").val();
-
-        $.post("http://localhost:8080/addQuestion", ques,
-            function(data, status) {
-                console.log("Data: " + data + "\nStatus: " + status);
-            });
+    $.get( "http://localhost:8080/", function( data ) {
+      $( ".result" ).html( data );
+      alert( "Load was performed." );
     });
+    // $('.addQuesBtn').click(function() {
+    //     ques.question = $(".question").val();
+    // });
 })
