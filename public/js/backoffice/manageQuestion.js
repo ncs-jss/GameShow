e96b6f90b0bf9619a9a18523a5036941d292b4c8
adi <adityaa803@gmail.com>
2016-10-01 05:10:15 +0530

    'added_manage_ques_module'

---
+++ public/js/backoffice/manageQuestion.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $.get("/getAllQuestion", function(data) {
+        var openHtml = '<tr>';
+        var closeHtml = '<td><div class="btn-group btn-justified pull-right"><button class="btn btn-success">Edit</button><button class="btn btn-danger deleteQuesBtn">Delete</button></div></td></tr>';
+        var allHtml;
+        var j;
+        for (var i = 0, l = data.length; i < l; i++) {
+            j = i + 1;
+            allHtml = '';
+            allHtml += '<td>' + j + '</td>\n';
+            allHtml += '<td>' + data[i].question + '</td>\n';
+            allHtml += '<td>' + data[i].technicalAnswer + '</td>\n';
+            allHtml += '<td>' + data[i].nonTechnicalAnswer + '</td>\n';
+            allHtml += '<td>' + data[i].hint + '</td>\n';
+            allHtml += '<td>' + data[i].level + '</td>\n';
+
+            allHtml = openHtml + allHtml + closeHtml;
+            $(".questionList").append(allHtml);
+            document.querySelectorAll(".questionList .deleteQuesBtn")[i].dataset.id = data[i]._id;
+        }
+        
+        // Delete feature
+        $('.deleteQuesBtn').click(function() {
+            var question_ID = $(this)[0].dataset.id;
+            console.log(question_ID);
+            $.post("/removeQuestion", question_ID, function(data, status) {
+                console.log("Data: " + data + "\nStatus: " + status);
+            });
+        });
+    });
+});
