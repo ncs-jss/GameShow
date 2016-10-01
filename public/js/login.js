abf057be4fc3b6299bc20bbb035569a4a9145d3d
adi <adityaa803@gmail.com>
2016-10-01 19:41:18 +0530

    'added_form_validation_#4_and_show_password'

--- public/js/login.js
+++ public/js/login.js
@@ -1,9 +1,41 @@
 $(document).ready(function() {
+    $(document).ready(function() {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
     var signup = {};
     var login = {};
     var modal = "";
+    var toggled = false;
+    $(".visibilityToggle").click(function() {
+        var input = $(this).prev();
+        if (!toggled) {
+            input.attr('type','text') ;
+            $(this).find(".fa-eye").addClass("hidden");
+            $(this).find(".fa-eye-slash").removeClass("hidden");
+            toggled = true;
+        }
+        else{
+            input.attr('type','password');
+            $(this).find(".fa-eye-slash").addClass("hidden");
+            $(this).find(".fa-eye").removeClass("hidden");
+            toggled = false;
+        }
+        input.trigger("focus");
+
+    })
+
+    function showError(elem) {
+        var elem = elem.find(".notifBox .notif-error");
+        elem.removeClass("hidden");
+        setTimeout(function() {
+            elem.addClass("hidden");
+        }, 600);
+    }
     $("#submit").click(function() {
         signup.email = $("#email").val();
+        var remail = new RegExp(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/),
+            rmobileNumber = new RegExp(/^[0-9]{1,10}$/),
+            rreferenceNo = new RegExp(/^[a-z0-9]+$/i);
         signup.mobileNumber = $("#mob").val();
         signup.password = $("#password").val();
         signup.avatar = "";
@@ -12,7 +44,7 @@ $(document).ready(function() {
         signup.referenceNo = $("#referenceNo").val();
         var cpassword = $("#cpassword").val();
         // console.log(signup);
-        if (cpassword === signup.password) {
+        if (cpassword === signup.password && signup.email.match(remail) && signup.mobileNumber.match(rmobileNumber) && signup.referenceNo.match(rreferenceNo)) {
             $.post("/register", signup,
                 function(data, status) {
                     console.log("Data: " + data + "\nStatus: " + status);
@@ -20,10 +52,12 @@ $(document).ready(function() {
                         $("#signupmodal").find(".notifBox .notif-correct").removeClass("hidden");
                         window.location = data.redirect;
                     } else {
-                        $("#signupmodal").find(".notifBox .notif-error").removeClass("hidden");
+                        showError($("#signupmodal"));
                     }
 
                 });
+        } else {
+            showError($("#signupmodal"));
         }
     });
     $("#login").click(function() {
@@ -31,7 +65,6 @@ $(document).ready(function() {
         $("#pass").val("a");
         login.emailOrNumber = $("#emailOrNumber").val();
         login.password = $("#pass").val();
-        console.log(login);
         $.post("/login", login,
             function(data, status) {
                 console.log("Data: " + data + "\nStatus: " + status);
@@ -43,16 +76,16 @@ $(document).ready(function() {
                 }
             });
     });
-    $(".checkModal1").click(function(){
+    $(".checkModal1").click(function() {
         modal = $("#submit");
     });
-    $(".checkModal2").click(function(){
+    $(".checkModal2").click(function() {
         modal = $("#login");
     });
     $(document).keydown(function(event) {
         var keyCode = (event.keyCode ? event.keyCode : event.which);
         if (keyCode == 13) {
-            if(modal){
+            if (modal) {
                 modal.trigger('click');
             }
         }
