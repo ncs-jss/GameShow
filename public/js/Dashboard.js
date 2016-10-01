b364381528a1efc1ffaf4a199a9d49cb5d873548
adi <adityaa803@gmail.com>
2016-10-01 11:43:47 +0530

    'added_enter_btn'

--- public/js/Dashboard.js
+++ public/js/Dashboard.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
-    $('.dropdown-toggle').dropdown()
-    new Circlebar({
-        element: "#circle-1",
-        type: "progress",
-        maxValue: "72"
+    $('.dropdown-toggle').dropdown();
+    $.get("/getQuestion",function(data){
+        console.log(data);
+        new Circlebar({
+            element: "#circle-1",
+            type: "progress",
+            maxValue: "72"
+        });
+        $(".ques").html();
     });
+    $(".submit_btn").click(function(){
+        $.post("/checkAnswer",{},function(){
+
+        })
+    });
+
 });
--- public/js/login.js
+++ public/js/login.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
         console.log(data);
     });
     */
+    var modal = "";
     $("#submit").click(function() {
         signup.email = $("#email").val();
         signup.mobileNumber = $("#mob").val();
@@ -47,5 +48,19 @@ $(document).ready(function() {
                     $("#loginmodal").find(".notifBox .notif-error").removeClass("hidden");
                 }
             });
+    });
+    $(".checkModal1").click(function(){
+        modal = $("#submit");
+    });
+    $(".checkModal2").click(function(){
+        modal = $("#login");
+    });
+    $(document).keydown(function(event) {
+        var keyCode = (event.keyCode ? event.keyCode : event.which);
+        if (keyCode == 13) {
+            if(modal){
+                modal.trigger('click');
+            }
+        }
     });
 });
