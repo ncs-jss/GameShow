da3b3d21353b1122197b79d0ef744428f6779856
adi <adityaa803@gmail.com>
2016-10-03 01:03:05 +0530

    badges and close btn

--- public/js/Dashboard.js
+++ public/js/Dashboard.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('.dropdown-toggle').dropdown();
+    $('[data-toggle="tooltip"]').tooltip();
     $.get("/getQuestion", function(data) {
         if (data.valid == 0) {
             window.location = data.redirect;
@@ -12,6 +12,7 @@ $(document).ready(function() {
 
             $.get("/user", function(data) {
                 localStorage.setItem('userdata',JSON.stringify(data));
+                console.log(data);
                 new Circlebar({
                     element: "#circle-1",
                     type: "progress",
@@ -22,6 +23,32 @@ $(document).ready(function() {
                 $(".levelText .level strong").html(data.level);
                 $(".levelText .score strong").html(data.score);
                 $(".avatarBox input").val(data.avatar);
+                var badges = data.badges;
+                console.log(badges);
+                // badges = [2,3];
+                var imgpath = "";
+                var elem = "";
+                badges.sort(function (a, b) {
+                  if (a.level > b.level) {
+                    return -1;
+                  }
+                  if (a.level < b.level) {
+                    return +1;
+                  }
+                  // a must be equal to b
+                  return 0;
+                });
+                console.log(badges);
+                $.each(badges,function(key,value){
+                    console.log(value);
+                    key += 1;
+                    imgpath = "/img/badges/" + value.name + ".png";
+                    elem = ".badges:nth-child(" + key + ")";
+                    console.log(elem);
+                    $(elem + " img").attr("src",imgpath);
+                    $(elem).tooltip().attr("data-original-title",value.name);
+                    $(elem + " p").html("Level " + value.level);
+                });
             });
         }
     });
@@ -29,19 +56,17 @@ $(document).ready(function() {
         $.post("/checkAnswer", { answer: $(".ans input").val() }, function(data) {
             console.log(data);
             if (data.valid == 1) {
-                console.log('test');
                 $(".notifBox").find(".notif-correct").removeClass("hidden");
                 setTimeout(function() {
                     window.location = data.redirect;
-                }, 4000);
+                }, 3000);
             } else {
-                console.log('testi');
                 console.log($(".notifBox").find(".notif-error"));
                 $(".notifBox").find(".notif-error").removeClass("hidden");
                 setTimeout(function() {
-                    $(".notifBox").find(".notif-error").addClass("animated fadeIn");
+                    $(".notifBox").find(".notif-error").addClass("animated fadeOut");
                     $(".notifBox").find(".notif-error").addClass("hidden");
-                }, 4000);
+                }, 2500);
             }
         });
     });
