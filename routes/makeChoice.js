3f229089f6e0b267e2062ece1e6af2f6912927b6
Devabhinav <abhinavpandey.1996@gmail.com>
2016-10-02 10:44:12 +0530

    minor integration changes

--- routes/makeChoice.js
+++ routes/makeChoice.js
@@ -5,8 +5,8 @@ var router = express.Router();
 router.post('/makeChoice', function (req,res) {
 	// body...
 	if(req.session.email) {
-		req.session.choice = req.body.email;
-		res.send({valid :1 , redirect : '/getQuestion'});
+		req.session.choice = 1;
+		res.send({valid :1 , redirect : '/'});
 	}
 });
 module.exports = router;
