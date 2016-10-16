dec05f7f4b1e4877ec037e8cde70efbe0410a1ab
Devabhinav <abhinavpandey.1996@gmail.com>
2016-10-16 20:21:53 +0530

    changes for keeping everything offline

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,12 @@ var router = express.Router();
 var User = require('../Models/userInfo');
 /* GET home page. */
 router.get('/', function(req, res) {
+
+ // remove code below to make things live
+
+ return res.redirect('/login');
+
+
 	console.log(req.session.email);
   if(req.session.email && req.session.email != "") {
   User.find({email_ID :req.session.email}).exec(function(err,result){
